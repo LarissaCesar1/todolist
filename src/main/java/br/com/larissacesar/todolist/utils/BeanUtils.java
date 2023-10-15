c8d56660726fbcc79b29ec42245d04bb83b0b886
Larissa Cesar <138222248+larissacesar-hotmart@users.noreply.github.com>
2023-10-15 15:11:53 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/larissacesar/todolist/utils/BeanUtils.java
@@ -0,0 +1,5 @@
+package br.com.larissacesar.todolist.utils;
+
+public class BeanUtils {
+
+}
---
+++ src/test/java/br/com/larissacesar/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.larissacesar.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
